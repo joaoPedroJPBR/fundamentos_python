5fedf1f2e58eab8b8310fe5af13a192b553186a7
João Pedro B. Rodrigues <joao.2020111956@unicap.br>
2025-03-28 23:26:50 -0300

    Commit com códigos iniciais.

---
+++ Aulas/aula1.py
@@ -0,0 +1,19 @@
+#Aula2 - Fundamentos em python
+print('Aula 2 - Fundamentos em python')
+
+# pi = 'pi'
+# print(pi)
+# print(type(pi)) 
+
+lista_num = [1,2,3,4,5, "teste", [1,4,5], 3.14, {2,"re"}, {"teste":2}]
+print(lista_num)
+
+dict_num = {
+    "nome": "João",
+    "idade": 25,
+    "cidade": "São Paulo",
+    "books": ["livro1", "livro2"]
+}
+print(dict_num)
+collection = set(dict_num)# muda a ordem das células
+print(collection)
---
+++ Listas de Exercícios/lista_1.py
@@ -0,0 +1,7 @@
+#primeira questão
+
+graus = int(input('Digite um angulo em graus:'))
+radianos = graus * 3.14 / 180
+
+print('O angulo em radianos é:', radianos)
+
